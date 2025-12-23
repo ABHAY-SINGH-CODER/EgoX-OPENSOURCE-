f5c4ae884527b897c6111e2a8cc0cf1d5cb77c08
kinam0252 <kinamplify@kaist.ac.kr>
2025-12-23 01:07:58 +0000

    resolve egoexo4d issue

--- scripts/infer_ego4d.sh
+++ scripts/infer_ego4d.sh
@@ -8,10 +8,7 @@ echo "Using GPUs: $GPU_IDS"
 
 #! Ego-Exo4D inference
 python3 infer.py \
-    --prompt ./example/egoexo4D/caption.txt \
-    --exo_video_path ./example/egoexo4D/exo_path.txt \
-    --ego_prior_video_path ./example/egoexo4D/ego_prior_path.txt \
-    --meta_data_file ./example/egoexo4D/camera_params.json \
+    --meta_data_file ./example/egoexo4D/meta.json \
     --depth_root ./example/egoexo4D/depth_maps/ \
     --model_path ./checkpoints/pretrained_model/Wan2.1-I2V-14B-480P-Diffusers \
     --lora_path ./checkpoints/EgoX/pytorch_lora_weights.safetensors \
